459877262daa9cf218de12829caa1ade8b674c3b
Nicolas Bages <nicolas.bages@weinto.com>
2013-05-07 06:05:26 +0200

    add returns and fix some bugs

--- classes/Hook/Core.php
+++ classes/Hook/Core.php
@@ -5,20 +5,24 @@ class Hook_Core {
 	protected static $_instance = null;
 
 	private $_actions = array();
+	private $_returns = array();
 
-	public static function instance() {
-		if (!isset(Hook_Core::$_instance)) {
+	public static function instance()
+	{
+		if (!isset(Hook_Core::$_instance))
+		{
 			self::$_instance = new Hook_Core();
 		}
+
 		return self::$_instance;
 	}
 
-	public function add_action($hook_name, $Class, $method, array $params = array(), array $hook_params = array()){
-		if(!isset($this->_actions[$hook_name])){
+	public function add_action($hook_name, $Class, $method, array $params = array(), array $hook_params = array())
+	{
+		if(!isset($this->_actions[$hook_name]))
+		{
 			$this->_actions[$hook_name] = array();
 		}
-
-
 
 		$this->_actions[$hook_name][] = array(
 			'class' => $Class,
@@ -27,46 +31,61 @@ class Hook_Core {
 		);
 	}
 
-	public function add_actions_from_config($hooker){
+	public function add_actions_from_config($hooker)
+	{
 		$actions = Kohana::$config->load('hooks.'.$hooker);
 
-		if($actions){
-			foreach ($actions as $action) {
+		if($actions)
+		{
+			foreach ($actions as $action)
+			{
 				$this->add_action(
 					$action['hook_name'],
-					$action['class'], 
-					$action['method'], 
+					$action['class'],
+					$action['method'],
 					( isset($action['params']) ? $action['params'] : array() )
 				);
 			}
 		}
 	}
 
-	public function do_action($Class, $method, array $params = array()){
-
-		if(!class_exists($Class)){
+	public function do_action($hook_name, $Class, $method, array $params = array())
+	{
+		if(!class_exists($Class))
+		{
 			throw new Kohana_Exception('The class '.$Class.' does not exist');
 		}
 
 		$Class = new $Class;
 
-		if(!method_exists($Class, $method)){
+		if(!method_exists($Class, $method))
+		{
 			throw new Kohana_Exception('The method '.$method.' does not exist in the class '.$Class);
 		}
 
-		call_user_func_array(array($Class, $method), $params);	
+		$this->_returns[$hook_name][] = call_user_func_array(array($Class, $method), $params);
 	}
 
-	public function do_all_actions($hook_name, array $hook_params = array()){
-		if( !isset($this->_actions[$hook_name]) || !is_array($hook_params) ){
+	public function do_all_actions($hook_name, array $hook_params = array())
+	{
+		if( !isset($this->_actions[$hook_name]) OR !is_array($hook_params) )
+		{
 			return;
 		}
-		foreach ($this->_actions[$hook_name] as $action) {
-			$action['params'] = array_reverse($action['params'], true);
-    		$action['params'][] = $hook_params; 
-    		$action['params'] = array_reverse($action['params'], true);
-			$this->do_action($action['class'], $action['method'], $action['params']);
+
+		foreach ($this->_actions[$hook_name] as $action)
+		{
+			$action['params']   = array_reverse($action['params'], true);
+			$action['params']['hook_params'] = '$hook_params';
+			$action['params']   = array_reverse($action['params'], true);
+
+			$this->do_action($hook_name, $action['class'], $action['method'], $action['params']);
 		}
+	}
+
+	public function get_returns($hook_name)
+	{
+		return isset($this->_returns[$hook_name]) ? $this->_returns[$hook_name] : null;
 	}
 
 	// TODO : develop has_action
